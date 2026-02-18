7362956e37a185cf72d8c10cbbc4233dbf242644
xyvielyons <111194732+xyvielyons@users.noreply.github.com>
2026-02-18 08:10:05 +0300

    remove ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,54 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env:{
+    NEXT_PUBLIC_MAIN_LINK: 'https://edusomo.com'
+  },
+  eslint: {
+      ignoreDuringBuilds: true,
+  },
+  typescript:{
+      ignoreBuildErrors:true
+  },
+
+  images: {
+    dangerouslyAllowSVG: true,
+    remotePatterns: [
+      
+      {
+        protocol: 'https',
+        hostname: '**.google.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.youtube.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.gstatic.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.pixabay.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'utfs.io',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.ufs.sh',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.dicebear.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
